29ba376aff32fbab816c754fdbfb1b5c0ed101d6
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-01-25 13:41:19 +0100

    front

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import Snake from "./Snake.js";
 import Fruits from "./Fruits.js";
 import Map from "./Map.js";
 
+const difficulty_btn = document.getElementById("difficulty--btn")
 const canvas = document.getElementById("canv");
 const ctx = canvas.getContext("2d");
 export const CANVAS_WIDTH = canvas.width = 500;
@@ -12,9 +13,13 @@ const snake = new Snake(50, 50);
 const fruit = new Fruits(10, 10);
 const map = new Map();
 
-console.log(map.width);
 const points_disp = document.getElementById("points");
 let points = 0;
+
+difficulty_btn.addEventListener("click",(e) =>{
+    let difficulty_choose_div = document.getElementById("difficulty--choose");
+    difficulty_choose_div.style.display="block";
+});
 
 function draw(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
